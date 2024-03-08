23ab7646fd08afa86060d34dff2a4f1e1562e204
James Blair <james@blair.nz>
2024-03-08 21:11:05 +1300

    Do not use NEXT_PUBLIC

--- src/components/recaptcha-provider.tsx
+++ src/components/recaptcha-provider.tsx
@@ -4,9 +4,7 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export function RecaptchaProvider({ children }: { children: React.ReactNode }) {
   return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
-    >
+    <GoogleReCaptchaProvider reCaptchaKey="6Lfn8N4oAAAAAFl8IlIhsh-ci869qxLklUeipLeT">
       {children}
     </GoogleReCaptchaProvider>
   );
