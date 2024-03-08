6da567d1261946fa729a1238285c93b514997ae9
James Blair <james@blair.nz>
2024-03-08 21:50:58 +1300

    Fix analytics domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
       <head>
         <PlausibleProvider
           domain="jamesblair.nz"
-          customDomain="analytics.koru.ws"
+          customDomain="https://analytics.koru.ws"
           selfHosted
         />
       </head>
