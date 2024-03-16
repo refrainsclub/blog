3a6297a49b1f0401b868cdcf50ffd70cd43fce9c
James Blair <james@blair.nz>
2024-03-17 02:15:13 +1300

    Update Google reCAPTCHA site key

--- src/components/recaptcha-provider.tsx
+++ src/components/recaptcha-provider.tsx
@@ -4,7 +4,7 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export function RecaptchaProvider({ children }: { children: React.ReactNode }) {
   return (
-    <GoogleReCaptchaProvider reCaptchaKey="6Lfn8N4oAAAAAFl8IlIhsh-ci869qxLklUeipLeT">
+    <GoogleReCaptchaProvider reCaptchaKey="6Le98JopAAAAALfc2CDVH8uMUKdgezlVcFT-MChG">
       {children}
     </GoogleReCaptchaProvider>
   );
